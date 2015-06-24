19230a65ec2a0db904753b7f701cf4fb59728e70
Kari H <kartahan@gmail.com>
2015-06-24 11:23:04 +0300

    JS tuning
    Moved all own javascript to separate file.

--- js/myscripts.js
+++ js/myscripts.js
@@ -14,5 +14,48 @@
             mode: 'fade',
             speed: 2000
         });
-    });
 
+        $(".emptyline")
+        {
+            document.getElementById("eline").innerHTML = '\n';
+        }
+        showHideText();
+    })
+
+    /**
+     * Expands or collapses text section when clicked.
+     */
+    function showHideText() {
+
+        var text = "Lisää...";
+        document.getElementById("demo").innerHTML = text;
+
+        $(".message").hide();
+        $(".message2").hide();
+
+        $(".readmore").click(function () {
+            // TODO Tee paremmin toi text/text2/demo/demo2-osuus
+            if ($(".message").is(":visible")) {
+                $(".message").hide();
+                text = "Lisää...";
+                document.getElementById("demo").innerHTML = text;
+            } else {
+                $(".message").show("slow");
+                text = "Sulje...";
+                document.getElementById("demo").innerHTML = text;
+            }
+        });
+        var text2 = "Lisää...";
+        document.getElementById("demo2").innerHTML = text2;
+        $(".readmore2").click(function () {
+            if ($(".message2").is(":visible")) {
+                $(".message2").hide();
+                text2 = "Lisää...";
+                document.getElementById("demo2").innerHTML = text2;
+            } else {
+                $(".message2").show("slow");
+                text2 = "Sulje...";
+                document.getElementById("demo2").innerHTML = text2;
+            }
+        });
+    };
